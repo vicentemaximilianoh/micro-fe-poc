8f67ac2b1c5097b1547943207d424b7d9c99f432
Maximiliano Vicente <vicente.maximilianoh@gmail.com>
2025-09-26 09:53:00 -0300

    chore: Add initial structure of Vite project with React + Typescript support

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Micro Frontend POC</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Welcome to your Vite + React + TypeScript project!
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
